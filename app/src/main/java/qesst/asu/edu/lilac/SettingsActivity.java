6daa602cac26fc9415ad699ba9cf03329891bbeb
Adric Rukkila <adric.rukkila@gmail.com>
2015-08-24 15:31:46 -0700

    Set summay and text for preferences options using a callback and defaults if blanked

--- app/src/main/java/qesst/asu/edu/lilac/SettingsActivity.java
+++ app/src/main/java/qesst/asu/edu/lilac/SettingsActivity.java
@@ -1,14 +1,22 @@
 package qesst.asu.edu.lilac;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /**
- * Created by amovi_000 on 8/24/2015.
+ * Application-wide settings
  */
 public class SettingsActivity extends PreferenceActivity
 {
+	private static final String PREF_NAME = "pref_custom_name_string";
+	private static final String PREF_MAC = "pref_custom_mac_string";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -16,7 +24,7 @@ public class SettingsActivity extends PreferenceActivity
 		getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
 	}
 
-	public static class SettingsFragment extends PreferenceFragment
+	public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener
 	{
 		@Override
 		public void onCreate(Bundle savedInstanceState)
@@ -25,6 +33,55 @@ public class SettingsActivity extends PreferenceActivity
 
 			// Load the preferences from an XML resource
 			addPreferencesFromResource(R.xml.preferences);
+
+			// Change the summary text to any saved values if we have them
+			SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext());
+			onSharedPreferenceChanged(sharedPreferences, PREF_NAME);
+			onSharedPreferenceChanged(sharedPreferences, PREF_MAC);
+		}
+
+		@Override
+		public void onResume()
+		{
+			super.onResume();
+			// Set up a listener whenever a key changes
+			getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onPause()
+		{
+			super.onPause();
+			// Set up a listener whenever a key changes
+			getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+		{
+			if (key.equals(PREF_NAME) || key.equals(PREF_MAC))
+			{
+				EditTextPreference editTextPreference = (EditTextPreference)findPreference(key);
+				String value = sharedPreferences.getString(key, "");
+				if (value.trim().isEmpty())
+				{
+					if (key.equals(PREF_NAME))
+					{
+						editTextPreference.setText(getString(R.string.pref_custom_name_hint));
+						editTextPreference.setSummary(getString(R.string.pref_custom_name_hint));
+					}
+					else if (key.equals(PREF_MAC))
+					{
+						editTextPreference.setText(getString(R.string.pref_custom_mac_hint));
+						editTextPreference.setSummary(getString(R.string.pref_custom_mac_hint));
+					}
+				}
+				else
+				{
+					editTextPreference.setText(value);
+					editTextPreference.setSummary(value);
+				}
+			}
 		}
 	}
 }
