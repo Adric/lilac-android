90b1a7456e36055af9eb23c1c0b090b037910f61
Adric Rukkila <adric.rukkila@gmail.com>
2015-08-23 17:23:19 -0700

    For some reason the activity files weren't committed. Update menus based on on flags from the arduino

---
+++ app/src/main/java/qesst/asu/edu/lilac/MainActivityFragment.java
@@ -0,0 +1,487 @@
+package qesst.asu.edu.lilac;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ValueFormatter;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.UUID;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements IMessageCallback
+{
+	private final static String TAG = "MainActivityFragment";
+
+	//private View mView;
+
+	private boolean mUseDummyData = false;
+
+	private boolean mEndMeasurement = false;
+
+	private int mSingleEntryIndex;
+
+
+	private BluetoothActivity mBluetooth;
+
+	private int mCallCount;
+
+	private GraphView mGraph;
+
+	private DummyData mDummyData = new DummyData();
+
+	private ModuleDataSet mDataSet;
+
+	/**
+	 * Class variables
+	 */
+	// List of files
+	private ArrayList<String> mFilenames;
+
+
+	// UI
+	private Button btnConnect = null;
+	private Button btnBegin = null;
+	private Button btnEnd = null;
+	private TextView txtReceived = null;
+	private Button btnClear = null;
+	private Button btnFlags = null;
+	private TextView lblVoc = null;
+	private TextView lblIsc = null;
+	private Menu mMenu = null;
+
+	private float mVoc;
+
+	private EnumSet<EFlag> mFlags;
+
+	public MainActivityFragment()
+	{
+		mBluetooth = new BluetoothActivity(this);
+		mFilenames = new ArrayList<String>();
+		mDataSet = new ModuleDataSet();
+		mSingleEntryIndex = 0;
+		mVoc = 0.f;
+		mCallCount = 0;
+		mFlags = EnumSet.noneOf(EFlag.class);
+		mFlags.add(EFlag.VOLTAGE); // DEBUG REMOVE ME
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.fragment_main, container, false);
+		//mView = view;
+
+		// set up the buttons
+		//btnVoltage = (Button) view.findViewById(R.id.btn_voltage);
+		//btnCurrent = (Button) view.findViewById(R.id.btn_current);
+		//btnSweep = (Button) view.findViewById(R.id.btn_sweep);
+		btnConnect = (Button) view.findViewById(R.id.btn_connect);
+		//btnAverage = (Button) view.findViewById(R.id.btn_average);
+		//btnDebug = (Button) view.findViewById(R.id.btn_debug_output);
+		btnBegin = (Button) view.findViewById(R.id.btn_begin);
+		btnEnd = (Button) view.findViewById(R.id.btn_end);
+		//btnContinuous = (Button) view.findViewById(R.id.btn_continuous);
+		//btnTemp = (Button) view.findViewById(R.id.btn_temp);
+		txtReceived = (TextView) view.findViewById(R.id.txt_received);
+		btnClear = (Button) view.findViewById(R.id.btn_clear);
+		btnFlags = (Button) view.findViewById(R.id.btn_flags);
+		lblVoc = (TextView) view.findViewById(R.id.lbl_voc);
+		lblIsc = (TextView) view.findViewById(R.id.lbl_isc);
+
+		lblVoc.setText("");
+		lblIsc.setText("");
+
+		// set non clickable in code since xml flags don't work
+		final boolean readOnly = true;
+		txtReceived.setFocusable(!readOnly);
+		txtReceived.setFocusableInTouchMode(!readOnly);
+		txtReceived.setClickable(!readOnly);
+		txtReceived.setLongClickable(!readOnly);
+		txtReceived.setCursorVisible(!readOnly);
+
+
+		btnConnect.setText("Connect");
+
+		btnConnect.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				if (!mBluetooth.isConnected())
+				{
+					mBluetooth.connect();
+					mCallCount = 0;
+					if (mBluetooth.isConnected())
+					{
+						btnConnect.setText("Disconnect");
+						// Get the flags back to set the menu entries
+						mFlags.add(EFlag.GET_FLAGS);
+						String flag_str = EFlag.toJsonString(mFlags);
+						//txtReceived.append("Sending flags: " + flag_str);
+						mBluetooth.write(flag_str);
+						mFlags.remove(EFlag.GET_FLAGS);
+					}
+				}
+				else
+				{
+					mFlags.remove(EFlag.RUN);
+					mBluetooth.write(EFlag.toJsonString(mFlags));
+					disconnect();
+					btnConnect.setText("Connect");
+				}
+			}
+		});
+
+
+		btnBegin.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				mEndMeasurement = false;
+				mFlags.add(EFlag.RUN);
+				mBluetooth.write(EFlag.toJsonString(mFlags));
+			}
+		});
+
+		btnEnd.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				mFlags.remove(EFlag.RUN);
+				mBluetooth.write(EFlag.toJsonString(mFlags));
+			}
+		});
+
+
+		btnFlags.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				mFlags.add(EFlag.GET_FLAGS);
+				mBluetooth.write(EFlag.toJsonString(mFlags));
+				mFlags.remove(EFlag.GET_FLAGS);
+			}
+		});
+
+		btnClear.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				txtReceived.setText("");
+			}
+		});
+
+		// Create the chart
+		mGraph = new GraphView((LineChart) view.findViewById(R.id.iv_curve), view);
+
+		// We have menu options
+		setHasOptionsMenu(true);
+
+		return view;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		inflater.inflate(R.menu.menu_fragment, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+
+		for (int i = 0; i < menu.size(); ++i)
+		{
+			MenuItem item = menu.getItem(i);
+			if (item.isCheckable())
+			{
+				item.setEnabled(mBluetooth.isConnected());
+			}
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (item.isCheckable())
+		{
+			if (mBluetooth == null || !mBluetooth.isConnected())
+			{
+				Toast.makeText(this.getActivity(), "Cannot change until connected!", Toast.LENGTH_SHORT).show();
+				return false;
+			}
+
+			/*
+			 * From: http://developer.android.com/guide/topics/ui/menus.html#checkable
+			 *
+			 * When a checkable item is selected, the system calls your respective
+			 * item-selected callback method (such as onOptionsItemSelected()).
+			 * It is here that you must set the state of the checkbox, because a
+			 * checkbox or radio button does not change its state automatically.
+			 * You can query the current state of the item
+			 * (as it was before the user selected it) with isChecked() and then
+			 * set the checked state with setChecked()
+			 */
+			int id = item.getItemId();
+			int old_flag_count = mFlags.size();
+
+			updateCheckedAndFlags(item, getEFlagFromMenuItem(item));
+
+			if (old_flag_count != mFlags.size())
+			{
+				mBluetooth.write(EFlag.toJsonString(mFlags));
+				return true;
+			}
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onPrepareOptionsMenu(Menu menu)
+	{
+		if (menu == null || mBluetooth.isConnected())
+		{
+			// Update checked-ness based on flags we've received back
+			for (int i = 0; i < menu.size(); ++i)
+			{
+				MenuItem item = menu.getItem(i);
+				//item.setEnabled(true);
+				item.setChecked(mFlags.contains(getEFlagFromMenuItem(item)));
+			}
+		}
+		else
+		{
+			Log.d(TAG, "onPrepareOptionsMenu when not connected!");
+		}
+	}
+
+	// Returns the flag associated with a menu item
+	private EFlag getEFlagFromMenuItem(MenuItem item)
+	{
+		if (item == null) return EFlag.NONE;
+
+		switch (item.getItemId())
+		{
+			case R.id.menu_item_voltage:
+				return EFlag.VOLTAGE;
+
+			case R.id.menu_item_current:
+				return EFlag.CURRENT;
+
+			case R.id.menu_item_temp:
+				return EFlag.TEMP;
+
+			case R.id.menu_item_sweep:
+				return EFlag.SWEEP;
+
+			case R.id.menu_item_continuous:
+				return EFlag.CONTINUOUS;
+
+			case R.id.menu_item_average:
+				return EFlag.AVERAGE;
+
+			case R.id.menu_item_debug:
+				return EFlag.DEBUG_OUTPUT;
+
+			default:
+				return EFlag.NONE;
+		}
+	}
+
+	private void updateCheckedAndFlags(MenuItem item, EFlag flag)
+	{
+		if (item == null || flag == null || flag == EFlag.NONE) return;
+
+		if (item.isChecked())
+		{
+			mFlags.remove(flag);
+			item.setChecked(false);
+		}
+		else
+		{
+			mFlags.add(flag);
+			item.setChecked(true);
+		}
+	}
+
+	@Override
+	public void call(ArrayList<String> arr)
+	{
+		mCallCount++;
+		Log.d("MessageData", "Call count: " + mCallCount);
+		String output = "";
+		for (String str : arr)
+		{
+			output += str;// + "\n";
+			txtReceived.append(output + "\n");
+
+			final String TAG = "JsonParsing";
+
+			try
+			{
+				// Ugly parsing, refactor!
+				JSONObject jObj = new JSONObject(str);
+				ModuleDataEntry mde = new ModuleDataEntry();
+				boolean add = false;
+				if (jObj.has("V"))
+				{
+					Log.d(TAG, "V found");
+					add = true;
+					mde.setVoltage(jObj.getDouble("V"));
+				}
+				if (jObj.has("I"))
+				{
+					Log.d(TAG, "I found");
+					add = true;
+					mde.setCurrent(jObj.getDouble("I"));
+				}
+				if (jObj.has("T"))
+				{
+					Log.d(TAG, "T found");
+					add = true;
+					mde.setTemp(jObj.getDouble("T"));
+				}
+				if (jObj.has("t"))
+				{
+					add = true;
+					Log.d(TAG, "t found");
+					mde.setTime(jObj.getLong("t"));
+				}
+				if (jObj.has("G"))
+				{
+					Log.d(TAG, "G found");
+					Log.d(TAG, "Current flags (int): " + jObj.getInt("G"));
+					EnumSet<EFlag> flags = EFlag.getEFlags(jObj.getInt("G"));
+					for (EFlag flag : flags)
+					{
+						Log.d(TAG, "Current flags (string): " + flag.toString());
+					}
+
+					if (flags.containsAll(mFlags))
+					{
+						Log.d(TAG, "All flags are set on the Arduino");
+					}
+
+					// Update the menu
+					getActivity().invalidateOptionsMenu();
+				}
+				if (jObj.has("F"))
+				{
+					Log.d(TAG, "F found");
+					if (jObj.getBoolean("F"))
+					{
+						Log.d(TAG, "Flag update successful!");
+					}
+					else
+					{
+						Log.d(TAG, "Flag update failed!");
+						if (jObj.has("G"))
+						{
+							Log.d(TAG, "Failed to update flags (int): " + jObj.getInt("G"));
+							EnumSet<EFlag> flags = EFlag.getEFlags(jObj.getInt("G"));
+							for (EFlag flag : flags)
+							{
+								Log.d(TAG, "Failed to update flags (string): " + flag.toString());
+							}
+						}
+					}
+				}
+				if (jObj.has("E"))
+				{
+					Log.d(TAG, "E found");
+					Log.e(TAG, "ERROR: " + jObj.getString("E"));
+				}
+				if (add)
+				{
+					Log.d(TAG, "Adding mde: " + mde.toString());
+					mDataSet.add(mde);
+				}
+			}
+			catch (Exception e)
+			{
+
+			}
+		}
+		//mDataSet.add(MessageData.stringToModuleData(output));
+		Log.e(TAG, output);
+
+		// Update graph with any data if we have some
+		//mDataSet.add(MessageData.stringToModuleData(output));
+
+		// Clear out what we get
+		arr.clear();
+
+		updateGraph();
+	}
+
+	public void updateGraph()
+	{
+		ModuleDataEntry entry = null;
+		if (mUseDummyData)
+		{
+			entry = mDummyData.getNextEntry();
+		}
+		else
+		{
+			if (mDataSet.isEmpty())
+			{
+				Log.e(TAG, "Trying to update graph with an empty dataset! Returning!");
+				return;
+			}
+			entry = mDataSet.get(mDataSet.size()-1);
+		}
+		mGraph.updateGraph(entry);
+	}
+
+	/**
+	 * Reset input and output streams and make sure socket is closed.
+	 * This method will be used during shutdown() to ensure that the connection is properly closed during a shutdown.
+	 * @return
+	 */
+	private void disconnect()
+	{
+		//mMessageSystem = null;
+
+		mBluetooth.disconnect();
+
+		txtReceived.append("Bluetooth Disconnected!\n");
+	}
+}
+
